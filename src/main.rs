21262dacb120c8c36130ee4015045ade5665dcc3
i3ima <hueputalo.rubin@gmail.com>
2021-09-04 16:34:03 +0300

    feat(core): new main function. yay!

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
-use std::{
-    env,
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use std::env;
 
-pub use watson::*;
 use std::time::Instant;
+pub use watson::*;
 
-fn parse_from_file() -> Vec<String> {
-    BufReader::new(File::open("./list.txt").expect("File not found"))
-        .lines()
-        .map(|line| line.unwrap())
-        .collect()
-}
-
-#[tokio::main]
-async fn main() {
+fn main() {
     let now = Instant::now();
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("Not enough arguments")
     }
     let username = &args[1];
-    let watson: WatsonData = Watson::new(username, parse_from_file());
-    watson
-        .check_hosts(&watson.hosts)
-        .await
-        .iter()
-        .for_each(|result| println!("{}", result));
+    let watson: WatsonData = WatsonBuilder::new(username).load_json(None).build();
+    watson.check_hosts(&watson.hosts);
     println!("Total execution time is {}ms", now.elapsed().as_millis())
 }
